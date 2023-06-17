5cfcfb77ce64d39c5d040f8ba2088ce822afc483
Shahaf Frank-Shapir <67577414+Shahaf-F-S@users.noreply.github.com>
2023-06-17 18:09:24 +0300

    Project Update

--- multithreading/process.py
+++ multithreading/process.py
@@ -3,24 +3,38 @@
 import datetime as dt
 import threading
 import time
+from abc import ABCMeta
 from typing import (
     Any, Optional, Dict, Callable, Iterable, Union
 )
 
-from represent import BaseModel
+from represent import BaseModel, Modifiers
 
 __all__ = [
     "Caller",
-    "Callers",
-    "multi_threaded_calls"
+    "CallDefinition",
+    "CallInfo",
+    "multi_threaded_call",
+    "multi_threaded_defined_call",
+    "wait_call_completion",
+    "CallWaitingInfo",
+    "ProcessInfo",
+    "find_caller"
 ]
 
 class Caller(BaseModel):
     """A class to represent a function caller object."""
 
+    modifiers = Modifiers(excluded=["thread"], force=True)
+
+    __slots__ = (
+        "target", "identifier", "args", "kwargs", "returns",
+        "thread", "start", "end", "complete", "called"
+    )
+
     def __init__(
             self,
-            caller: Callable,
+            target: Callable,
             identifier: Optional[Any] = None,
             args: Optional[Iterable[Any]] = None,
             kwargs: Optional[Dict[str, Any]] = None,
@@ -29,19 +43,19 @@ class Caller(BaseModel):
         """
         Defines the class attributes.
 
-        :param caller: The function to call.
+        :param target: The function to call.
         :param identifier: The identifier of the call.
         :param args: The positional arguments.
         :param kwargs: The keyword arguments.
         :param returns: The returned response.
         """
 
-        self.caller = caller
+        self.target = target
         self.returns = returns
 
         self.args = args or ()
         self.kwargs = kwargs or {}
-        self.identifier = identifier or self.caller
+        self.identifier = identifier or self.target
 
         self.thread: Optional[threading.Thread] = None
 
@@ -69,7 +83,7 @@ class Caller(BaseModel):
 
         self.called = True
 
-        self.returns = self.caller(*self.args, **self.kwargs)
+        self.returns = self.target(*self.args, **self.kwargs)
 
         self.complete = True
 
@@ -103,49 +117,275 @@ class Caller(BaseModel):
     # end reset
 # end Caller
 
-class Callers(BaseModel):
-    """A class to represent a function caller object."""
+def find_caller(callers: Iterable[Caller], identifier: Any) -> Caller:
+    """
+    Finds the caller object by its identifier
 
-    def __init__(self, callers: Iterable[Caller]) -> None:
+    :param callers: The callers in which to search.
+    :param identifier: The identifier of the caller to return.
+
+    :return: The matching caller object.
+    """
+
+    for caller in callers:
+        if caller.identifier == identifier:
+            return caller
+        # end if
+    # end for
+
+    raise ValueError(
+        f"Cannot find a caller object with the identifier: "
+        f"{identifier}. valid identifiers are: "
+        f"{', '.join(str(caller.identifier) for caller in callers)}"
+    )
+# end find_caller
+
+class CallDefinition(BaseModel):
+    """A class to represent the call definition."""
+
+    WAIT = True
+    RESET_BEFORE = True
+    RESET_AFTER = False
+
+    SLEEP = 0.005
+
+    __slots__ = "wait", "reset_before", "reset_after", "sleep"
+
+    def __init__(
+            self,
+            wait: Optional[bool] = None,
+            reset_before: Optional[bool] = None,
+            reset_after: Optional[bool] = None,
+            sleep: Optional[Union[int, float, dt.timedelta]] = None
+    ) -> None:
         """
         Defines the class attributes.
 
-        :param callers: The call objects.
+        :param wait: The value to wait.
+        :param reset_before: The value to reset before running.
+        :param reset_after: The value to reset after running.
+        :param sleep: The time for sleeping.
         """
 
-        self.callers = list(callers)
+        if wait is None:
+            wait = self.WAIT
+        # end if
+
+        if sleep is None:
+            sleep = self.SLEEP
+        # end if
+
+        if reset_before is None:
+            reset_before = self.RESET_BEFORE
+        # end if
+
+        if reset_after is None:
+            reset_after = self.RESET_AFTER
+        # end if
+
+        self.wait = wait
+        self.reset_before = reset_before
+        self.reset_after = reset_after
+        self.sleep = sleep
+    # end __init__
+# end CallDefinition
+
+class ProcessInfo(BaseModel, metaclass=ABCMeta):
+    """A class to contain the info of a call to the callers."""
+
+    modifiers = Modifiers(excluded=["thread"], properties=True)
+
+    __slots__ = 'callers', 'start', 'end', 'definition'
+
+    def __init__(
+            self,
+            callers: Iterable[Caller],
+            start: dt.datetime,
+            end: dt.datetime,
+            definition: CallDefinition
+    ) -> None:
+        """
+        Defines the class attributes.
+
+        :param callers: The callers object.
+        :param start: The starting time for the call.
+        :param end: The ending time for the call.
+        :param definition: The call definition object.
+        """
+
+        self.callers = callers
+        self.definition = definition
+
+        self.start = start
+        self.end = end
     # end __init__
 
-    def caller(self, identifier: Any) -> Caller:
+    @property
+    def time(self) -> dt.timedelta:
         """
-        Finds the caller object by its identifier
+        Returns the time duration of the call.
 
-        :param identifier: The identifier of the caller to return.
-
-        :return: The matching caller object.
+        :return: The call time.
         """
 
-        for caller in self.callers:
-            if caller.identifier == identifier:
-                return caller
-            # end if
-        # end for
+        return self.end - self.start
+    # end time
+# end CallInfo
 
-        raise ValueError(
-            f"Cannot find a caller object with the identifier: "
-            f"{identifier}. valid identifiers are: "
-            f"{', '.join(str(caller.identifier) for caller in self.callers)}"
+
+class CallWaitingInfo(ProcessInfo):
+    """A class to contain the info of a call to the callers."""
+# end CallWaitingInfo
+
+class CallInfo(ProcessInfo):
+    """A class to contain the info of a call to the callers."""
+
+    __slots__ = "waiting",
+
+    def __init__(
+            self,
+            callers: Iterable[Caller],
+            start: dt.datetime,
+            waiting: CallWaitingInfo,
+            end: dt.datetime,
+            definition: CallDefinition
+    ) -> None:
+        """
+        Defines the class attributes.
+
+        :param callers: The callers object.
+        :param start: The starting time for the call.
+        :param end: The ending time for the call.
+        :param definition: The call definition object.
+        """
+
+        super().__init__(
+            callers=callers, start=start,
+            end=end, definition=definition
         )
-    # end caller
-# end Callers
 
-def multi_threaded_calls(
-        callers: Callers,
-        wait: Optional[bool] = True,
-        reset_before: Optional[bool] = True,
-        reset_after: Optional[bool] = False,
-        sleep: Optional[Union[int, float, dt.timedelta]] = 0.005
-) -> None:
+        self.waiting = waiting
+    # end __init__
+# end CallInfo
+
+def validate_callers(data: Any) -> Iterable[Caller]:
+    """
+    Validates the data as callers.
+
+    :param data: The data to validate.
+
+    :return: The valid callers data.
+    """
+
+    try:
+        if not all(isinstance(value, Caller) for value in data):
+            raise ValueError
+        # end if
+
+        return data
+
+    except (TypeError, ValueError) as e:
+        raise type(e)(
+            f"Callers must be an iterable of "
+            f"{Caller} objects, not: {data}."
+        ) from e
+    # end try
+# end validate_callers
+
+def wait_call_completion(
+        callers: Iterable[Caller], definition: CallDefinition
+) -> CallWaitingInfo:
+    """
+    Waits for the calls to complete.
+
+    :param callers: The call objects for the functions.
+    :param definition: The call definition object.
+
+    :return: The waiting results.
+    """
+
+    start = dt.datetime.now()
+
+    while (
+        definition.wait and
+        not all(caller.complete for caller in callers)
+    ):
+        if isinstance(definition.sleep, dt.timedelta):
+            sleep = definition.sleep.total_seconds()
+
+        else:
+            sleep = definition.sleep
+        # end if
+
+        time.sleep(sleep)
+    # end while
+
+    end = dt.datetime.now()
+
+    return CallWaitingInfo(
+        callers=callers, start=start,
+        end=end, definition=definition
+    )
+# end wait_call_completion
+
+def multi_threaded_defined_call(
+        callers: Iterable[Caller],
+        definition: Optional[CallDefinition] = None
+) -> CallInfo:
+    """
+    Calls the functions with the callers.
+
+    :param callers: The call objects for the functions.
+    :param definition: The call definition object.
+
+    :return: The call results.
+    """
+
+    start = dt.datetime.now()
+
+    callers = validate_callers(data=callers)
+
+    if definition is None:
+        definition = CallDefinition()
+    # end if
+
+    if definition.reset_before:
+        for caller in callers:
+            caller.reset()
+        # end for
+    # end if
+
+    for caller in callers:
+        caller.thread = threading.Thread(target=caller)
+
+        caller.thread.start()
+    # end for
+
+    waiting = wait_call_completion(
+        callers=callers, definition=definition
+    )
+
+    if definition.reset_after:
+        for caller in callers:
+            caller.reset()
+        # end for
+    # end if
+
+    end = dt.datetime.now()
+
+    return CallInfo(
+        callers=callers, start=start, end=end,
+        definition=definition, waiting=waiting
+    )
+# end multi_threaded_defined_call
+
+def multi_threaded_call(
+        callers: Iterable[Caller],
+        wait: Optional[bool] = None,
+        reset_before: Optional[bool] = None,
+        reset_after: Optional[bool] = None,
+        sleep: Optional[Union[int, float, dt.timedelta]] = None
+) -> CallInfo:
     """
     Calls the functions with the callers.
 
@@ -154,31 +394,15 @@ def multi_threaded_calls(
     :param reset_before: The value to reset before running.
     :param reset_after: The value to reset after running.
     :param sleep: The time for sleeping.
+
+    :return: The call results.
     """
 
-    if reset_before:
-        for caller in callers.callers:
-            caller.reset()
-        # end for
-    # end if
-
-    for caller in callers.callers:
-        caller.thread = threading.Thread(target=caller)
-
-        caller.thread.start()
-    # end for
-
-    while wait and not all(caller.complete for caller in callers.callers):
-        if isinstance(sleep, dt.timedelta):
-            sleep = sleep.total_seconds()
-        # end if
-
-        time.sleep(sleep)
-    # end while
-
-    if reset_after:
-        for caller in callers.callers:
-            caller.reset()
-        # end for
-    # end if
-# end multi_threaded_calls
+    return multi_threaded_defined_call(
+        callers=callers,
+        definition=CallDefinition(
+            wait=wait, reset_after=reset_after,
+            reset_before=reset_before, sleep=sleep
+        )
+    )
+# end multi_threaded_call
--- test.py
+++ test.py
@@ -1,7 +1,45 @@
 # test.py
 
+import random
+import time
+
+from multithreading import Caller, multi_threaded_call
+
+def slow_function(minimum: int, maximum: int) -> int:
+    """
+    A function to generate a random int and wait for that amount of seconds.
+
+    :param minimum: The minimum limit for the generation.
+    :param maximum: The maximum limit for the generation.
+
+    :return: The random number.
+    """
+
+    number = random.randint(minimum, maximum)
+
+    time.sleep(number)
+
+    return number
+# end slow_function
+
+CALLS = 1
+MIN = 1
+MAX = 2
+
 def main() -> None:
     """A function to run the main test."""
+
+    callers = [
+        Caller(
+            target=slow_function,
+            kwargs=dict(minimum=MIN, maximum=MAX)
+        )
+        for _ in range(CALLS)
+    ]
+
+    results = multi_threaded_call(callers=callers)
+
+    print(results)
 # end main
 
 if __name__ == "__main__":
