ac984b1faa435a31984fc63dfe6baac66caccf84
Shahaf Frank-Shapir <shahaffrs@gmail.com>
2024-01-20 21:01:10 +0200

    Project Update

--- README.md
+++ README.md
@@ -2,3 +2,50 @@
 
 > A python module for creating multithreading processes easily, in a more Pythonic way.
 
+## example
+
+```python
+import time
+
+from multithreading import Caller, multi_threaded_call
+
+def slow_function(number: int, delay: float) -> dict[str, int | float]:
+
+    for i in range(number):
+        time.sleep(delay)
+
+    return dict(number=number, delay=delay)
+
+CALLS = 50
+DELAY = 0.01
+NUMBER = 100
+
+callers = [
+    Caller(
+        target=slow_function,
+        kwargs=dict(delay=DELAY, number=NUMBER)
+    ) for _ in range(CALLS)
+]
+
+s = time.time()
+
+multi_threaded_call(callers)
+
+e = time.time()
+
+print("multi-threading: ", e - s)
+
+s = e
+
+all(slow_function(*caller.args, **caller.kwargs) for caller in callers)
+
+e = time.time()
+
+print("single-threading: ", e - s)
+```
+
+output
+```
+multi-threading: 1.0473840236663818
+single-threading: 52.53497314453125
+```
--- test.py
+++ test.py
@@ -1,80 +1,44 @@
 # test.py
 
-import random
 import time
 
 from multithreading import Caller, multi_threaded_call
 
-data = []
-
-def slow_function(
-        minimum: int = None,
-        maximum: int = None,
-        number: int = None,
-        delay: float = None
-) -> dict[str, int | float]:
-    """
-    A function to generate a random int and wait for that amount of seconds.
-
-    :param minimum: The minimum limit for the generation.
-    :param maximum: The maximum limit for the generation.
-    :param number: a random number.
-    :param delay: A random delay.
-
-    :return: The random number.
-    """
-
-    if number is None:
-        if None in (minimum, maximum):
-            raise ValueError(
-                "minimum and maximum must be defined when number is not."
-            )
-        # end if
-
-        number = random.randint(minimum, maximum)
-    # end if
+def slow_function(number: int, delay: float) -> dict[str, int | float]:
 
     for i in range(number):
-        if delay is None:
-            delay = random.randint(0, 10) / 100
-        # end if
-
         time.sleep(delay)
 
-        data.append(number)
-        data.sort()
-    # end for
-
-    return dict(
-        minimum=minimum, maximum=maximum,
-        number=number, delay=delay
-    )
-# end slow_function
+    return dict(number=number, delay=delay)
 
 CALLS = 50
-MIN = 10
-MAX = 100
+DELAY = 0.01
+NUMBER = 100
 
 def main() -> None:
-    """A function to run the main test."""
 
     callers = [
         Caller(
             target=slow_function,
-            kwargs=dict(minimum=MIN, maximum=MAX)
+            kwargs=dict(delay=DELAY, number=NUMBER)
         ) for _ in range(CALLS)
     ]
 
-    results = multi_threaded_call(callers=callers)
+    s = time.time()
 
-    for result in sorted(
-        [result for result in results.results.values()],
-        key=lambda res: res.time.time
-    ):
-        print(result)
-    # end for
-# end main
+    multi_threaded_call(callers)
 
-if __name__ == "__main__":
+    e = time.time()
+
+    print(e - s)
+
+    s = e
+
+    all(slow_function(*caller.args, **caller.kwargs) for caller in callers)
+
+    e = time.time()
+
+    print(e - s)
+
+if __name__ == '__main__':
     main()
-# end if
